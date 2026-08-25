b3ce287b502a1bf4287797539f3c61b40c55812c
KauaGabriell <kauagabriell64@gmail.com>
2026-08-25 15:33:39 -0300

    chore(dev): configure local PostgreSQL environment

---
+++ apps/api/src/config/env.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(["development", "test", "production"]),
+  PORT: z.coerce.number().int().positive(),
+  DATABASE_URL: z.url(),
+});
+
+const envConfig = envSchema.parse(process.env);
+
+export { envConfig };
